15df1a65bf7e49617a3dc370b6021eaa793ee6b7
EC2 Default User <ec2-user@ip-172-31-30-5.ec2.internal>
2024-06-09 08:34:09 +0000

    Update FastAPI and Nginx configurations, add README

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from typing import List
 from datetime import datetime
@@ -11,6 +12,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:8000",
+    "http://100.28.146.73"
 ]
 
 app.add_middleware(
@@ -40,6 +42,13 @@ async def add_entry(entry: GuestbookEntry):
 async def get_entries():
     return guestbook
 
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    with open("/home/ec2-user/guest-book/static/index.html", "r") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
+
 @app.delete('/guestbook/{entry_id}')
 async def delete_entry(entry_id: int):
     global guestbook
@@ -49,4 +58,5 @@ async def delete_entry(entry_id: int):
             return {"message": f"Entry with ID {entry_id} deleted successfully"}
     raise HTTPException(status_code=404, detail="Entry not found")
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="/home/ec2-user/guest-book/static"), name="static")
+
---
+++ start_server.sh
@@ -0,0 +1,1 @@
+source /home/ec2-user/guest-book/myenv/bin/activate && /home/ec2-user/guest-book/myenv/bin/uvicorn main:app --host 0.0.0.0 --port 8000
